4e5bf57bf8570a110a7c07d67d562393bf04c037
FiorellaSaa <fsaavedras.s@hotmail.com>
2022-10-04 09:07:19 -0500

    Avance con la función mdLinks

--- src/mdLinks.js
+++ src/mdLinks.js
@@ -1,9 +1,34 @@
+const {
+  routeExists,
+  convertRouteToAbsolute,
+  fileExtension,
+  getLinks,
+  storeLinks,
+  makeHttpRequest,
+  // readDirectory,
+} = require('./index');
 
-
-function mdLinks("./prueba/prueba.md"){
-    .then(links => {
-        // => [{ href, text, file }, ...]
-    })
-    .catch(console.error);
+function mdLinks(path, options) {
+  return new Promise((resolve, reject) => {
+    // Función para verificar si existe la ruta
+    if (routeExists(path) === false) {
+      reject('the path is invalid');
+    }
+    // Función que convierte la ruta en absoluta
+    const absoluteRoute = convertRouteToAbsolute(path);
+    // Función que verifica si el archivo es .md
+    const arraysFile = fileExtension(absoluteRoute);
+    // Función que lee el archivo y valida
+    makeHttpRequest(arraysFile)
+      .then((res) => {
+        resolve(res);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
 }
-  
+console.log(mdLinks('./prueba/prueba.md'))
+module.exports = {
+  mdLinks,
+};
