56e9a0ade0043eec2d9664ba69d72122d944169b
FiorellaSaa <fsaavedras.s@hotmail.com>
2022-10-16 23:07:45 -0500

    Se modifica nombre de carpeta

--- src/index.js
+++ src/index.js
@@ -1,164 +1,48 @@
-/* eslint-disable no-unreachable-loop */
-const fs = require('fs');
-const path = require('path');
-const axios = require('axios');
-
-const ruta1 = './prueba/prueba.md';
-const ruta2 = './prueba/prueba2.md';
-const ruta3 = './prueba/prueba.text';
-const rutaDir = './prueba';
-const rutaAbsolute = 'c:\\Users\\Fiorella\\Proyecto 4-mdLinks\\LIM018-md-links';
-
-// Verificar si existe la ruta
-function routeExists(route) {
-  const routeVerify = fs.existsSync(route);
-  if (routeVerify) {
-    return route;
-  }
-  return 'Err: the route does not exist';
-}
-// console.log(routeExists(ruta1));
-// console.log(routeExists(ruta2));
-
-// Verificar si la ruta es abosluta
-function routeAbsolute(route) {
-  const routeAbsoluteVerify = path.isAbsolute(route);
-  if (routeAbsoluteVerify) {
-    return route;
-  }
-  return 'The path is relative';
-}
-// console.log(routeAbsolute(ruta1));
-// console.log(routeAbsolute(rutaAbsolute));
-
-// convertir una ruta relativa a abosluta
-function convertRouteToAbsolute(route) {
-  return path.resolve(route);
-}
-// console.log("Route converted to absolute", convertRouteToAbsolute(ruta1));
-
-// Extensión .md
-function fileExtension(route) {
-  const routeExt = path.extname(route);
-  if (routeExt === '.md') {
-    return routeExt;
-  }
-  return 'It is not .md extension';
-}
-// console.log(fileExtension('./prueba/prueba1.md'));
-
-// Leyendo data del archivo prueba.md-síncrono
-function readContent(route) {
-  try {
-    const readContentFile = fs.readFileSync(route, 'utf-8');
-    return readContentFile;
-  } catch (error) {
-    return '';
-  }
-}
-// console.log(readContent(ruta1));
-// Verificar si es un directorio
-const isDirectory = (route) => fs.lstatSync(route).isDirectory();
-// console.log(isDirectory(rutaDir));
-
-// Verificar si es un archivo
-const isFile = (route) => fs.lstatSync(route).isFile();
-// console.log(isFile(rutaDir));
-
-// Leer archivos de un directorio
-const readDirectory = (route) => fs.readdirSync(route);
-// console.log(readDirectory(rutaDir));
-// [ 'prueba.html', 'prueba.md', 'prueba.text', 'prueba1.md' ]
-
-// Recursión para obtener los archivos
-const recursionToObtainFiles = (route) => {
-  const storeFiles = [];
-  if (isFile(route)) {
-    storeFiles.push(route);
-    return storeFiles;
-  }
-  const readFilesOfDirectory = readDirectory(route);
-  for (let i = 0; i < readFilesOfDirectory.length; i++) {
-    const readArrayFiles = path.join(route, readFilesOfDirectory[i]);
-    storeFiles.push(recursionToObtainFiles(readArrayFiles));
-  }
-  return storeFiles.flat();
-};
-// console.log(recursionToObtainFiles(rutaDir));
-/* [
-  'prueba\\prueba.html',
-  'prueba\\prueba.md',
-  'prueba\\prueba.text',
-  'prueba\\prueba1.md'
-] */
-
-// Función para obtener los links
-function getLinks(route) {
-  const methodReadFile = readContent(route);
-  const regularExpression = /\[(.+)\]\((https?:\/\/.+)\)/gi;
-  // const regularExpression = /(\[(.*?)\])?\(http(.*?)\)/gm
-  const arrayLinks = methodReadFile.match(regularExpression);
-  return arrayLinks;
-}
-// console.log(getLinks('./prueba/prueba1.md'));
-
-// Función para almacenar los links en un array
-function storeLinks(arrayLinks, route) {
-  const newArray = [];
-  if (arrayLinks.length > 0) {
-    // eslint-disable-next-line no-plusplus
-    for (let i = 0; i < arrayLinks.length; i++) {
-      const firstCut = arrayLinks[i].indexOf(']');
-      const textLink = arrayLinks[i].slice(1, firstCut);
-      const urlLink = arrayLinks[i].slice(firstCut + 2, -1);
-      const object = {
-        text: textLink,
-        href: urlLink,
-        file: route,
-      };
-      newArray.push(object);
-    }
-    return newArray;
-  }
-  return 'No links in the file';
-}
-// console.log(storeLinks(getLinks(ruta1), ruta1));
-
-const makeHttpRequest = (arrayObject) => {
-  const getArray = [];
-  for (let i = 0; i < arrayObject.length; i++) {
-    const element = arrayObject[i];
-    const runAxios = axios
-      .get(element.href)
-      .then((response) => ({
-        ...element,
-        status: response.status,
-        message: response.statusText,
-      }))
-      .catch((error) => ({
-        ...element,
-        status: 404,
-        message: 'fail',
-      }));
-    getArray.push(runAxios);
-  }
-  return Promise.all(getArray);
-};
-/* makeHttpRequest(storeLinks(getLinks(ruta1), ruta1)).then((response) => {
-  console.log(response);
-}); */
-
-module.exports = {
+/* eslint-disable max-len */
+const {
   routeExists,
-  routeAbsolute,
   convertRouteToAbsolute,
   fileExtension,
-  readContent,
-  isDirectory,
-  isFile,
-  readDirectory,
-  recursionToObtainFiles,
   getLinks,
   storeLinks,
+  recursionToObtainFiles,
   makeHttpRequest,
+} = require('./mdLinks');
+
+function mdLinks(route, options) {
+  return new Promise((resolve, reject) => {
+    // Función para verificar si existe la ruta
+    const verifyRoute = routeExists(route);
+    // Función que convierte la ruta en absoluta
+    const absoluteRoute = convertRouteToAbsolute(verifyRoute);
+    // Función que lee el archivo y valida
+    if (verifyRoute) {
+      if (recursionToObtainFiles(absoluteRoute)) {
+        const allArrays = recursionToObtainFiles(absoluteRoute);
+        const newArraysMd = allArrays.filter((fileRoute) => fileExtension(fileRoute) === '.md');
+        const allStoreLinks = [];
+        newArraysMd.forEach((fileRoute) => {
+          allStoreLinks.push(storeLinks(getLinks(fileRoute), fileRoute));
+        });
+        if (options.validate === true) {
+          resolve(makeHttpRequest(allStoreLinks.flat()));
+          // .then((response) => {
+          // console.log(response);
+          // resolve('resolver lo que haga falta');
+          // });
+        } else {
+          resolve(allStoreLinks.flat());
+        }
+      }
+    } else {
+      reject(new Error('Err: the route does not exist'));
+    }
+  });
+}
+/* mdLinks('./prueba', { validate: true }).then((resolve) => {
+  console.log(resolve);
+}).catch((error) => { console.log(error); }); */
+
+module.exports = {
+  mdLinks,
 };
