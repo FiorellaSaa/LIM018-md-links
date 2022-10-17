fab051f2afa91d862e24f3a977268f6c0b40ca14
FiorellaSaa <fsaavedras.s@hotmail.com>
2022-10-17 10:58:06 -0500

    Se agrega chalk

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,8 @@
 #!/usr/bin/env node
-// const chalk = require('chalk');
+const chalk = require('chalk');
 const { mdLinks } = require('./index');
 const { totalStatsVerify, uniqueLinksStats, brokenLinks } = require('./stats');
-// const args = process.argv.slice(2);
-// print hello world provided args
-// console.log(`Hello world ${args}`);
 
-// console.log(args);
 // const process.argv = ['node', 'file.js', ‘x’, ‘y’];
 // arg[0] node route --> node 'C:\\Program Files\\nodejs\\node.exe',
 // arg[1] md-links route --> 'cli.js'
@@ -14,63 +10,63 @@ const { totalStatsVerify, uniqueLinksStats, brokenLinks } = require('./stats');
 // arg[3] 'y' options --validate or --stats --> secondarg
 
 const args = process.argv;
-// const pathArg = args.filter((x) => !['--stats', '--validate', '--help'].includes(x))[2];
 const initialArg = process.argv.slice(2);
 const route = args[2];
 const optionValidate = initialArg.includes('--validate');
 const optionStats = initialArg.includes('--stats');
 const help = initialArg.includes('--help');
-// const help = args.includes('--help');
+
 if (args.length <= 2) {
-  console.log(`
+  console.log(chalk.yellow(`
   ══════════════════════════════════════════════════════════════════
           ⚠ Enter a path or type md-links --help to help you
   ══════════════════════════════════════════════════════════════════
-  `);
+  `));
 } else if (help) {
-  console.log(`
+  console.log(chalk.green(`
   ═══════════════════════════════════════════════════════════════════════════════════════════════
-    💻Ingrese: md-links <path> [options]
-
-    md-links <path>                    ║ Links found with their path, text and file
-    md-links <path> --validate         ║ Links found with their path, text, status, and ok/fail
-    md-links <path> --stats            ║ Displays the total number of links and unique links 
-    md-links <path> --validate --stats ║ Displays the total links, unique links and broken links
+  💻Type:
+   ✔ md-links <path>                    ║ Links found with their path, text and file
+   ✔ md-links <path> --validate         ║ Links found with their path, text, status, and ok/fail
+   ✔ md-links <path> --stats            ║ Displays the total number of links and unique links 
+   ✔ md-links <path> --validate --stats ║ Displays the total links, unique links and broken links
   ════════════════════════════════════════════════════════════════════════════════════════════════
-   `);
+   `));
 } else {
   mdLinks(route, { stats: optionStats, validate: optionValidate })
     .then((arrayLinks) => {
       if (optionStats && !optionValidate) {
         console.log(`
         ※ Stats ※
-      Total links: ${totalStatsVerify(arrayLinks)};
-      Unique Links: ${uniqueLinksStats(arrayLinks)};
+      Total links: ${chalk.blue(totalStatsVerify(arrayLinks))};
+      Unique Links: ${chalk.magenta(uniqueLinksStats(arrayLinks))};
       `);
       } else if (optionValidate && optionStats) {
         console.log(`
         ※ Validate && Stats ※
-      Total links: ${totalStatsVerify(arrayLinks)};
-      Unique Links: ${uniqueLinksStats(arrayLinks)};
-      Broken links: ${brokenLinks(arrayLinks)};
+      Total links: ${chalk.blue(totalStatsVerify(arrayLinks))};
+      Unique Links: ${chalk.magenta(uniqueLinksStats(arrayLinks))};
+      Broken links: ${chalk.cyan(brokenLinks(arrayLinks))};
       `);
       } else {
         arrayLinks.forEach((link) => {
           if (!optionStats && !optionValidate) {
             console.log(`
           ※ Links founds ※
-          href: ${link.href};
-          text: ${link.text};
-          file: ${link.file};
+          href: ${chalk.green(link.href)};
+          text: ${chalk.green(link.text)};
+          file: ${chalk.green(link.file)};
           `);
           } else {
             console.log(`
           ※ status links ※
-          href: ${link.href};
-          text: ${link.text};
-          file: ${link.file};
-          message: ${link.message};
-          status: ${link.status};
+          href: ${chalk.green(link.href)};
+          text: ${chalk.green(link.text)};
+          file: ${chalk.green(link.file)};
+          message: ${
+  link.message === 'OK' ? chalk.yellow(link.message) : chalk.red(link.message)
+};
+          status: ${chalk.yellow(link.status)};
           `);
           }
         });
